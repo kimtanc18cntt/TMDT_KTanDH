901f1d383feec63f7e2d7a025f9bcf9341141f9b
lekimtan0610@gmail.com <kimtan667>
2021-11-08 08:57:01 +0700

    rolex hello

--- wordpress/wp-content/plugins/index.php
+++ wordpress/wp-content/plugins/index.php
@@ -1,2 +1,19 @@
-<?php
-// Silence is golden.
+
+         <?php
+/*
+Plugin Name: mykt_plg
+
+Description: Just another contact form plugin. Simple but flexible.
+Author: Le Kim Tan
+
+Domain Path: /languages/
+Version: 5.5.1
+*/
+function hello($t){
+    $text = 'Rolex';  
+		 $text = preg_replace('/(\b[a-z])/i','<span style="color:red;">\1</span>',$text);  
+		 echo $text;
+}
+add_filter('the_content','hello');
+add_filter('the_title','hello');
+?>
